23d105897e34e56280f28344b9fd5f54da1563ad
Nitu Singh <nitu.sin122@gmail.com>
2017-06-12 19:22:47 +0530

    Add files via upload

---
+++ Assign2.1.js
@@ -0,0 +1,4 @@
+//MyPens - Anonymous function
+var MyPens = function (penName, quantity) {
+    return ("I have " + quantity + " " + penName + " pens");
+};
---
+++ Assign2.1.ts
@@ -0,0 +1,4 @@
+//MyPens - Anonymous function
+let MyPens = (penName: string, quantity: number): string => {
+    return(`I have ${quantity} ${penName} pens`);
+}
